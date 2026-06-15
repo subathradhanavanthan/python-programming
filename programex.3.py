f7b8444e0b644ba2007cb6f1239e511c122d16a9
subathradhanavanthan <subathradhanavanthan@gmail.com>
2026-06-15 17:53:20 +0530

    Add files via upload

---
+++ programex.3.py
@@ -0,0 +1,15 @@
+a=int(input("enter a number"))
+b=int(input("enter a number"))
+c=input("enter a operation(+,-,*,/):")          
+if c=="+":
+    print("addition=",a+b)      
+elif c==("-"):
+   print("subtraction=",a-b)
+elif c==("*"):
+   print("multiplication=",a*b)
+elif c==("/"):
+    print("division=",a/b)
+else :
+    print("no operation here")
+    
+   
